020a6516f8f9cd39c86d04a431d722fd006796b1
Staking Agency <contact@staking.agency>
2024-03-05 15:26:27 +0200

    improved LP tests

--- tests/modules_tests/lp_tests.rs
+++ tests/modules_tests/lp_tests.rs
@@ -1,3 +1,4 @@
+use salsa::SalsaContract;
 use salsa::{exchanges::arbitrage::ArbitrageModule, helpers::HelpersModule};
 use salsa::exchanges::lp::LpModule;
 
@@ -115,28 +116,52 @@ fn test_add_remove_imbalanced_lp() {
     nonce += BLOCKS_PER_EPOCH;
     set_block_nonce(&mut world, nonce);
     delegate_test(&mut world, DELEGATOR1_ADDRESS_EXPR, &trade_amount, false, true);
+    delegate_all_test(&mut world);
     sell_on_xexchange(&mut world, DELEGATOR1_ADDRESS_EXPR, &trade_amount, &rust_biguint!(ONE_EGLD));
 
-    // disable lp module
+    // disable lp module and take profit
     let salsa_whitebox = WhiteboxContract::new(SALSA_ADDRESS_EXPR, salsa::contract_obj);
-    world.whitebox_call(
+    let mut egld_to_delegate = rust_biguint!(0);
+    let mut egld_to_undelegate = rust_biguint!(0);
+    world.whitebox_call_check(
         &salsa_whitebox,
         ScCallStep::new()
-            .from(OWNER_ADDRESS_EXPR),
+            .from(OWNER_ADDRESS_EXPR)
+            .no_expect(),
         |sc| {
             sc.set_lp_inactive();
-        }
+            sc.take_lp_profit();
+            sc.call_reduce_egld_to_delegate_undelegate();
+            egld_to_delegate = num_bigint::BigUint::from_bytes_be(sc.egld_to_delegate().get().to_bytes_be().as_slice());
+            egld_to_undelegate = num_bigint::BigUint::from_bytes_be(sc.egld_to_undelegate().get().to_bytes_be().as_slice());
+        },
+        |_| {}
     );
 
-    // check if legld in custody and reserves are restored
+    // delegate and undelegate everything, then check if we suffered from IL
+    nonce += BLOCKS_PER_EPOCH;
+    set_block_nonce(&mut world, nonce);
+    if egld_to_delegate >= rust_biguint!(ONE_EGLD) {
+        delegate_all_test(&mut world);
+    }
+    if egld_to_undelegate >= rust_biguint!(ONE_EGLD) {
+        undelegate_all_test(&mut world);
+        nonce += UNBOND_PERIOD * BLOCKS_PER_EPOCH;
+        set_block_nonce(&mut world, nonce);
+        withdraw_all_test(&mut world);
+        compute_withdrawn_test(&mut world);
+    }
     world.whitebox_query(
         &salsa_whitebox, |sc| {
             let (egld_balance, legld_balance) = sc.get_sc_balances();
-            assert!(egld_balance >= to_managed_biguint(&liquidity_amount));
-            assert!(legld_balance >= to_managed_biguint(&liquidity_amount));
+            let amount = to_managed_biguint(&liquidity_amount);
+            assert!(sc.lp_state().get() == State::Inactive);
+            assert!(egld_balance >= amount);
+            assert!(legld_balance == amount);
         }
     );
 
+    // check if there are no more LPs in SC
     check_esdt_balance(&mut world, SALSA_ADDRESS_EXPR, ONEDEX_LP_EXPR, &rust_biguint!(0));
     check_esdt_balance(&mut world, SALSA_ADDRESS_EXPR, XEXCHANGE_LP_EXPR, &rust_biguint!(0));
 }
